d50f72817e501560b756e265c4d54366b21ff80b
Gyárfás Balázs <gyarfasb02@gmail.com>
2022-08-02 02:31:15 +0200

    szenzorok

--- car.js
+++ car.js
@@ -5,71 +5,76 @@ class Car{
         this.width=width;
         this.height=height;
 
-        this.speed=0
-        this.acceleration=0.2
-        this.maxSpeed=3
-        this.friction=0.05
-        this.angle=0 //az oldalsó mozgás realisztikusabb
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
 
-        this.controls=new Controls()
+        this.sensor=new Sensor(this);
+        this.controls=new Controls();
     }
-    update(){
-        this.#move()
+
+    update(roadBorders){
+        this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move(){
         if(this.controls.forward){
-            this.speed+=this.acceleration
+            this.speed+=this.acceleration;
         }
         if(this.controls.reverse){
-            this.speed-=this.acceleration
+            this.speed-=this.acceleration;
         }
 
         if(this.speed>this.maxSpeed){
-            this.speed=this.maxSpeed
+            this.speed=this.maxSpeed;
         }
         if(this.speed<-this.maxSpeed/2){
-            this.speed=-this.maxSpeed/2
+            this.speed=-this.maxSpeed/2;
         }
+
         if(this.speed>0){
-            this.speed-=this.friction
+            this.speed-=this.friction;
         }
         if(this.speed<0){
-           this.speed+=this.friction
+            this.speed+=this.friction;
         }
-        if(Math.abs(this.speed)<this.friction){//az autó nem mozog pixeleket
-            this.speed=0
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
         }
 
-        if(this.speed!=0){ //ebbe foglaljuk bele az alsó 2 if részt
-            const flip=this.speed>0?1:-1//nem forog egy helyben az autó
-        
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
 
-        if(this.controls.left){
-            this.angle+=0.03*flip//meg kell szorozni a flip értékkel, hogy ne forogjon egy helyben
-        }
-        if(this.controls.right){
-            this.angle-=0.03*flip
-        }
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
-        this.x-=Math.sin(this.angle)*this.speed
-        this.y-=Math.cos(this.angle)*this.speed
-        //this.y-=this.speed
-    }
+
     draw(ctx){
-        ctx.save()
-        ctx.translate(this.x,this.y)
-        ctx.rotate(-this.angle)
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
 
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.rect(
             -this.width/2,
             -this.height/2,
             this.width,
             this.height
-        )
-        ctx.fill()
+        );
+        ctx.fill();
 
-        ctx.restore()
+        ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
--- controls.js
+++ controls.js
@@ -1,46 +1,45 @@
 class Controls{
     constructor(){
-        this.forward=false
-        this.left=false
-        this.right=false
-        this.reverse=false
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
         this.#addKeyboardListeners();
     }
+
     #addKeyboardListeners(){
-        document.onkeydown=(event)=>{
+        document.onkeydown=(event)=>{ //ha az arrowfunnctiont használod, akkor vissza tud nyúlni a fentiekhez
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=true
-                    break
+                    this.left=true;
+                    break;
                 case "ArrowRight":
-                    this.right=true
-                    break
+                    this.right=true;
+                    break;
                 case "ArrowUp":
-                    this. forward=true
-                    break
+                    this.forward=true;
+                    break;
                 case "ArrowDown":
-                    this.reverse=true
-                    break       
+                    this.reverse=true;
+                    break;
             }
-            //console.table(this)
         }
-        document.onkeyup=(event)=>{ //ha az arrowfunciont használod, akkor a funkció vissza tud nyúlni a fentiekhez
+        document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=false
-                    break
+                    this.left=false;
+                    break;
                 case "ArrowRight":
-                    this.right=false
-                    break
+                    this.right=false;
+                    break;
                 case "ArrowUp":
-                    this. forward=false 
-                    break
+                    this.forward=false;
+                    break;
                 case "ArrowDown":
-                    this.reverse=false
-                    break       
+                    this.reverse=false;
+                    break;
             }
-            //console.table(this)
         }
     }
 }
